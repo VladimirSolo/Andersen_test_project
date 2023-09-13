cd5be0df300d0a7649a214b9d4b59475c669da6d
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-14 00:51:48 +0600

    feat: add router dom and test react lazy suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
 import './index.scss'
+import { AboutLazy } from './About/AboutLazy';
+import { MainLazy } from './Main/MainLazy';
 
 const App = () => {
     return (
         <div className="app">
-          Hello Vovan
+          <Link to={'./'}>Main</Link>
+          <Link to={'./about'}>About</Link>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path={'/'} element={<MainLazy/>}/>
+                <Route path={'/about'} element={<AboutLazy/>}/>
+            </Routes>
+          </Suspense>
         </div>
     );
 };
