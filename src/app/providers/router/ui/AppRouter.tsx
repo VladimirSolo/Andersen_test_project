fc85d6526ccb51e1e8d41e34dd2c4ded022e26be
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-15 05:40:15 +0600

    feat: create not found page component

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Main } from "pages/Main";
 import { Login } from "pages/Login";
+import { NotFoundPage } from "pages/NotFoundPage";
 
 export default function AppRouter() {
   return (
@@ -9,6 +10,7 @@ export default function AppRouter() {
           <Routes>
               <Route path="/" element={<Main />} />
               <Route path="/login" element={<Login />} />
+              <Route path="*" element={<NotFoundPage />} />
           </Routes>
       </Suspense>
   );
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import s from "./NotFoundPage.module.scss";
+
+export const NotFoundPage = () => (
+    <div className={s.not}>
+        <h1>404 Page Not Found</h1>
+        <Link to="./" className={s.back}>Back to Main page</Link>
+    </div>
+);
+export default NotFoundPage;
