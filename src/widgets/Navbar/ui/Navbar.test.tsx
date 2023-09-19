a674b0d136ba268c2636251a0ee9aff280cd0f5c
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-20 01:50:59 +0600

    fix: try fix test navbar

--- src/widgets/Navbar/ui/Navbar.test.tsx
+++ src/widgets/Navbar/ui/Navbar.test.tsx
@@ -1,29 +1,62 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { useTypedDispatch, useTypedSelector } from "app/providers/store/config/hooks";
+import { logout } from "features/Auth/model";
+
 import Navbar from "./Navbar";
 
-describe("navbar", () => {
-  test("withs one parameters", () => {
-    render(
-        <Router>
-            <Navbar />
-        </Router>,
-    );
-    expect(screen.getByTestId("navbar")).toBeInTheDocument();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+
+const useDispatchMock = useTypedDispatch as jest.Mock;
+const useSelectorhMock = useTypedSelector as jest.Mock;
+
+describe("Navbar Component", () => {
+  beforeEach(() => {
+    useDispatchMock.mockReturnValue(jest.fn()); // Mock the dispatch function
+    useSelectorhMock.mockReturnValue({ uid: "user123" }); // Mock the user data
   });
 
-  test("Navbar should render with the logo and links", () => {
+  it("renders Navbar when user is logged in", () => {
     render(
-        <Router>
+        <BrowserRouter>
             <Navbar />
-        </Router>,
+        </BrowserRouter>,
     );
 
-    expect(screen.getByText("ovies")).toBeInTheDocument();
+    expect(screen.getByTestId("navbar")).toBeInTheDocument();
+    expect(screen.getByText("Movies")).toBeInTheDocument();
     expect(screen.getByText("Favorites")).toBeInTheDocument();
     expect(screen.getByText("History")).toBeInTheDocument();
     expect(screen.getByText("Logout")).toBeInTheDocument();
+  });
+
+  it("renders Navbar when user is not logged in", () => {
+    useSelectorhMock.mockReturnValue({ uid: null }); // Mock user as not logged in
+    render(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>,
+    );
+
+    expect(screen.getByTestId("navbar")).toBeInTheDocument();
+    expect(screen.getByText("Movies")).toBeInTheDocument();
     expect(screen.getByText("Login")).toBeInTheDocument();
     expect(screen.getByText("Sign Up")).toBeInTheDocument();
+  });
+
+  it("calls the logout function when Logout is clicked", async () => {
+    render(
+        <BrowserRouter>
+            <Navbar />
+        </BrowserRouter>,
+    );
+
+    fireEvent.click(screen.getByText("Logout"));
+
+    expect(logout).toHaveBeenCalled();
   });
 });
