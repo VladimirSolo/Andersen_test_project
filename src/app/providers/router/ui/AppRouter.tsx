68ba17ccdcbfd2681de1d58d5f19110284a455db
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-24 02:21:46 +0600

    fix: rename favorite to favoritepage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -6,7 +6,7 @@ import { SignUpPage } from "pages/SignUpPage";
 import { NotFoundPage } from "pages/NotFoundPage";
 import { PageLoader } from "widgets/PageLoader";
 import { ProtectedRoute } from "widgets/ProtectedRoute";
-import { Favorites } from "pages/Favorites";
+import { FavoritesPage } from "pages/FavoritesPage";
 import { History } from "pages/History";
 import { FilmDetails } from "shared/ui";
 import { SearchPage } from "pages/SearchPage";
@@ -20,7 +20,7 @@ export default function AppRouter() {
                   <Route path="/signup" element={<SignUpPage />} />
               </Route>
               <Route element={<ProtectedRoute isAuthenticated />}>
-                  <Route path="/favorites" element={<Favorites />} />
+                  <Route path="/favorites" element={<FavoritesPage />} />
                   <Route path="/history" element={<History />} />
               </Route>
               <Route path="/:id" element={<FilmDetails />} />
