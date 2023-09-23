ae8d6775a820a2db6080c9df59a6b8dcfee5c625
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-24 02:41:58 +0600

    fix: add text information if not have favorites

--- src/features/FirebaseDB/ui/Favorites.tsx
+++ src/features/FirebaseDB/ui/Favorites.tsx
@@ -19,13 +19,19 @@ const Favorites = () => {
   }, [getFavoritesMovies]);
 
   return (
-      <div className={s.favorites}>
-          {films.map((film) => (
-              <Link className={s.picture} key={film.imdbID} to={`/${film.imdbID}`}>
-                  <img className={s.img} src={film.Poster} alt={film.Title} />
-              </Link>
-          ))}
-      </div>
+    !favoriteFilms
+      ? (
+          <div className={s.info}>Please add movies</div>
+      )
+      : (
+          <div className={s.favorites}>
+              {films.map((film) => (
+                  <Link className={s.picture} key={film.imdbID} to={`/${film.imdbID}`}>
+                      <img className={s.img} src={film.Poster} alt={film.Title} />
+                  </Link>
+              ))}
+          </div>
+      )
   );
 };
 
