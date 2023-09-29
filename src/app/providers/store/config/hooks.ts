6b718850f92eebbdcd3ed4b7267d898de0486ebf
Vladimir Solo <freeman.wushu@mail.ru>
2023-09-29 18:00:17 +0600

    fix: hooks any to undefined

--- src/app/providers/store/config/hooks.ts
+++ src/app/providers/store/config/hooks.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import { AnyAction } from "redux";
 import { ThunkDispatch } from "redux-thunk";
 import { RootState } from "./store";
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useTypedDispatch: () => ThunkDispatch<RootState, any, AnyAction> = useDispatch;
+export const useTypedDispatch: () => ThunkDispatch<RootState, undefined, AnyAction> = useDispatch;
